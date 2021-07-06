1c3484f82b155c3c88ac720b2b6701c6fac347a2
Denis Alves <dee.alves2@gmail.com>
2021-07-05 23:49:09 -0300

    Trabalhando com tratamento de exceções

--- app/src/main/java/com/digitalinnovation/myfirstmobileproject/MainActivity.kt
+++ app/src/main/java/com/digitalinnovation/myfirstmobileproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.digitalinnovation.myfirstmobileproject
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val MIN_AVG = 6
@@ -12,32 +13,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val currentThread = Thread.currentThread()
+        currentThread.setUncaughtExceptionHandler {_, throwable->
+            val x = throwable.message
+            val cause = throwable.cause
+        }
+        ""
         val calculateButton = calculateBtn
         val result = textViewResult
 
         calculateBtn.setOnClickListener {
-            val gradeOne = Integer.parseInt(editTextStudentGradeOne.text.toString())
-            val gradeTwo = Integer.parseInt(editTextStudentGradeTwo.text.toString())
-            val avg = (gradeOne + gradeTwo) / 2
-            val abs = Integer.parseInt(editTextAttendStudent.text.toString())
+            try {
+                val gradeOne = Integer.parseInt(editTextStudentGradeOne.text.toString())
+                val gradeTwo = Integer.parseInt(editTextStudentGradeTwo.text.toString())
+                val avg = (gradeOne + gradeTwo) / 2
+                val abs = Integer.parseInt(editTextAttendStudent.text.toString())
 
-            if (avg >= MIN_AVG && abs <= MAX_ABS) {
-                result.setTextColor(Color.GREEN)
-                result.text = """
+                if (avg >= MIN_AVG && abs <= MAX_ABS) {
+                    result.setTextColor(Color.GREEN)
+                    result.text = """
                     Parabéns!! 
                     Você foi aprovado.
                     Nota Final: $avg 
                     Número de faltas: $abs
                 """.trimIndent()
-            } else {
-                result.setTextColor(Color.RED)
-                result.text = """
+                    Log.i("click", "Deu certo!")
+                } else {
+                    Log.e("click", "Deu Ruim!")
+                    result.setTextColor(Color.RED)
+                    result.text = """
                     Sinto muito...
                     Você foi reprovado.
                     Nota Final: $avg
-                    Número de faltas: $abs
+                    Número de faltas: $abs  
                 """.trimIndent()
+                }
+            } catch (e: NumberFormatException) {
+                Log.i("Error!", "Valores informados estão vazios")
             }
         }
     }
